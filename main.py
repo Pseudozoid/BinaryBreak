fe82835456cdda514955debfb369fa3ce517dd1f
Rishi-k-s <rishikrishna.sr@gmail.com>
2024-04-21 14:23:02 +0530

    added to the main proj from test

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
+import serial.tools.list_ports
 from roboflow import Roboflow
 import json
+
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+count  = 0
+
 rf = Roboflow(api_key="otKMUXAELXaU2XZsT77M")
 project = rf.workspace().project("biomedical-wastes")
 model = project.version(3).model
 
-# infer on a local image
 json_response = model.predict("./syringe.jpg", confidence=40, overlap=30).json()
 
-threshold = 0.8
-for prediction in json_response["predictions"]:
-    if prediction["confidence"] > threshold:
-        # Do something specific
-        print("Syringe detected!")
-        break;
+portsList = []
+# portvar = ""
+for onePort in ports:
+    portsList.append(str(onePort))
+    print (str(onePort))
 
-# save the JSON response to a file
-with open('output.json', 'w') as f:
-    json.dump(json_response, f)
+val = "9"
 
+for x in range(0,len(portsList)):
+    if portsList[x].startswith("COM" + str(val)):
+        portvar = "COM" + str(val)
+        print (portvar)
+
+serialInst.baudrate = 115200
+serialInst.port = portvar
+serialInst.open()
+
+isTest= True;
+while isTest:
+    threshold = 0.8
+    for prediction in json_response["predictions"]:
+        if prediction["confidence"] > threshold:
+            # Do something specific
+            print("Syringe detected!")
+            cmd = "INJD"
+            serialInst.write(cmd.encode('utf-8'))
+            isTest=False
+            # save the JSON response to a file
+            with open('output.json', 'w') as f:
+                json.dump(json_response, f)
+            break;
--- test.py
+++ test.py
@@ -1,48 +1,4 @@
-import serial.tools.list_ports
-from roboflow import Roboflow
-import json
 
-ports = serial.tools.list_ports.comports()
-serialInst = serial.Serial()
-
-rf = Roboflow(api_key="otKMUXAELXaU2XZsT77M")
-project = rf.workspace().project("biomedical-wastes")
-model = project.version(3).model
-
-json_response = model.predict("./syringe.jpg", confidence=40, overlap=30).json()
-
-portsList = []
-# portvar = ""
-for onePort in ports:
-    portsList.append(str(onePort))
-    print (str(onePort))
-
-val = "9"
-
-for x in range(0,len(portsList)):
-    if portsList[x].startswith("COM" + str(val)):
-        portvar = "COM" + str(val)
-        print (portvar)
-
-serialInst.baudrate = 115200
-serialInst.port = portvar
-serialInst.open()
-
-isTest= True;
-while isTest:
-    threshold = 0.8
-    for prediction in json_response["predictions"]:
-        if prediction["confidence"] > threshold:
-            # Do something specific
-            print("Syringe detected!")
-            cmd = "INJD"
-            serialInst.write(cmd.encode('utf-8'))
-            isTest=False
-            break;
-
-    # save the JSON response to a file
-    with open('output.json', 'w') as f:
-        json.dump(json_response, f)
 
 
 
@@ -54,7 +10,26 @@ while isTest:
 
 
 
+from roboflow import Roboflow
+import json
+rf = Roboflow(api_key="otKMUXAELXaU2XZsT77M")
+project = rf.workspace().project("biomedical-wastes")
+model = project.version(3).model
+count = 0;
 # infer on a local image
+json_response = model.predict("./syringe.jpg", confidence=40, overlap=30).json()
+
+threshold = 0.8
+for prediction in json_response["predictions"]:
+    if prediction["confidence"] > threshold:
+        # Do something specific
+        print("Syringe detected!")
+        break;
+
+# save the JSON response to a file
+with open('output.json', 'w') as f:
+    json.dump(json_response, f)
+
 
 
 
